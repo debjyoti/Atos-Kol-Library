60d0449af8bb14d90c8381643269ba1ec3c639fe
Debjyoti Majumder <debjyoti.majumder@gmail.com>
2013-04-01 23:49:49 +0530

    Can login with email also. Change is layout.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :das_id, :email, :password, :password_confirmation, :remember_me, :name, :phone_number, :manager, :max_book_count, :seat_number
+  attr_accessible :das_id, :email, :password, :password_confirmation, :remember_me, :name, :phone_number, :manager, :max_book_count, :seat_number, :login
   # attr_accessible :title, :body
+  attr_accessor :login
   has_many :books
   has_many :blocked_books, class_name: "Book", foreign_key: "blocked_by_id"
   validates :name, presence: true
@@ -37,4 +38,12 @@ class User < ActiveRecord::Base
     UserMailer.approval_notify(self).deliver
   end
 
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(das_id) = :value OR lower(email) = :value", { :value => login.downcase } ]).first
+    else
+      where(conditions).first
+    end
+  end
 end
